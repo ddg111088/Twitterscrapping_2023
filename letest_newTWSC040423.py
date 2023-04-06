fd4abb6940e794338a8a5e80797c5c88b737a9dc
ddg111088 <88524335+ddg111088@users.noreply.github.com>
2023-04-06 22:36:11 +0530

    Update letest_newTWSC040423.py

--- letest_newTWSC040423.py
+++ letest_newTWSC040423.py
@@ -139,10 +139,12 @@ else:
 st.info("Total Tweets Scraped " + str(len(tweets_df)-1))
 
 
-filtered_df = dataframe_explorer(tweets_df) 
+ 
 if st.sidebar.button("Show Tweets"):
     st.write(tweets_df)
-    st.dataframe(filtered_df, use_container_width=True)
+
+filtered_df = dataframe_explorer(tweets_df)
+st.dataframe(filtered_df, use_container_width=True)
     
    
 # DOWNLOAD AS CSV
